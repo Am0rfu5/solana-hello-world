aefd31842f414db61e0f402cdba1c87bb8a87316
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2022-11-11 19:32:56 -0500

    basic Next.js app

---
+++ app/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Your Solana Web3 App!
+        </h1>
+      </div>
+    </div>
+  );
+}
